0afb8c11074f2172a037093238a9ebe9aacebd99
Yareli Gonzalez <112053255+xxkyw01@users.noreply.github.com>
2024-01-08 15:54:20 -0600

    Add files via upload

---
+++ cadenadesdeteclado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadenadesdeteclado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string nombre; 
+
+            //imprime la pregunta y funcion desde teclado 
+            Console.WriteLine("Hola , Cual es tu nombre?");
+            nombre = Console.ReadLine();
+
+            //resultado
+            Console.WriteLine("hola {0} , un placer ",nombre);
+
+            Console.WriteLine("");
+
+        }
+    }
+}
